28af1d95a8f3876ead7624af352fade50f5e0b26
dx <505707606@qq.com>
2019-04-27 12:17:43 +0800

    多模块打包顺序测试完毕

---
+++ qujiang-api/src/main/java/MainApplication.java
@@ -0,0 +1,6 @@
+public class MainApplication {
+    public static void main(String[] args) {
+        HellowMultiQujiangService hellowMultiQujiangService = new HellowMultiQujiangService();
+        hellowMultiQujiangService.printHello();
+    }
+}
---
+++ qujiang-service/src/main/java/HellowMultiQujiangService.java
@@ -0,0 +1,5 @@
+public class HellowMultiQujiangService {
+    public void printHello(){
+        System.out.println("曲江service模块输出了这条内容");
+    }
+}
